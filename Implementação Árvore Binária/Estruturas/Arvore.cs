d1ea78b2f32dc49dca45bafb24ccf148a7eb70c1
Breno Robert <brenorobert@ucl.br>
2022-09-08 13:03:26 -0300

    Formatação

--- Implementação Árvore Binária/Estruturas/Arvore.cs
+++ Implementação Árvore Binária/Estruturas/Arvore.cs
@@ -1,121 +1,124 @@
 using System;
 using Operations;
 using Enums;
-namespace Estruturas {
-    
-  public class Arvore 
-  {
-    
-    public No Pai { get; set; }
-  
-    public Arvore(No pai)
+namespace Estruturas
+{
+
+    public class Arvore
     {
-      this.Pai = pai;
-    }
-  
-    public Arvore() {  }
 
-    public No BuscaNo (Dado dado, No noAtual)
-    {
-      if( ArvoreVazia() ) 
-      {
-        throw new ArgumentException("Arvore está vazia.");
-      }
+        public No Pai { get; set; }
 
-      if(noAtual.Equals(dado))
-      {
-        return noAtual;
-      }
-
-      Lado lado = Operador.LadoEscolher(dado, noAtual);
-      
-      if( lado.Equals(Lado.Esquerdo) )
-      {
-        return BuscaNo(dado, noAtual.Esquerdo);
-      } 
-      if( lado.Equals(Lado.Direito) )
-      {
-        return BuscaNo(dado, noAtual.Direito);
-      }
-      return null;
-      
-    }
-
-     public void InserirNo (No noInserir, No noAtual) {
-      
-       if( ArvoreVazia() ) 
+        public Arvore(No pai)
         {
-          this.Pai = noInserir;
-           return ;
+            this.Pai = pai;
         }
-       Dado dado = noInserir.Dado;    
 
-      if(noAtual.Equals(dado))
-      {
-        noAtual.Esquerdo = noInserir;
-        return ;
-      }
+        public Arvore() { }
 
-       
-      Lado lado = Operador.LadoEscolher(dado, noAtual);
-       bool inserir = false;
-      if((noAtual.Esquerdo is null && lado == Lado.Esquerdo) || (noAtual.Direito is null && lado == Lado.Direito))
-      {
-        inserir = true;
-      }
-     switch(lado)
-     {
-       case Lado.Direito: 
-         if(inserir) 
-         {
-           noAtual.Direito = new No(dado);
-           break;
-         }
-         InserirNo(noInserir, noAtual.Direito);
-         break;
-       case Lado.Esquerdo: 
-         if(inserir) 
-         {
-           noAtual.Esquerdo = new No(dado);
-           break;
-         }
-         InserirNo(noInserir, noAtual.Esquerdo);
-         break; 
-       default: break;
-     }
-       
-      return ;
-     }
+        public No BuscaNo(Dado dado, No noAtual)
+        {
+            if (ArvoreVazia())
+            {
+                throw new ArgumentException("Arvore está vazia.");
+            }
 
-    public void PercorreArvore(No noAtual) 
-    {
-      if( ArvoreVazia() ) 
-      {
-        throw new ArgumentException("Arvore está vazia.");
-      }
-      Console.WriteLine(noAtual);
-      
-      if(!(noAtual.Esquerdo is null)) 
-      {
-        PercorreArvore(noAtual.Esquerdo);
-      }
-      if(!(noAtual.Direito is null)) 
-      {
-        PercorreArvore(noAtual.Direito);
-      }
-      
+            if (noAtual.Equals(dado))
+            {
+                return noAtual;
+            }
+
+            Lado lado = Operador.LadoEscolher(dado, noAtual);
+
+            if (lado.Equals(Lado.Esquerdo))
+            {
+                return BuscaNo(dado, noAtual.Esquerdo);
+            }
+            if (lado.Equals(Lado.Direito))
+            {
+                return BuscaNo(dado, noAtual.Direito);
+            }
+            return null;
+
+        }
+
+        public void InserirNo(No noInserir, No noAtual)
+        {
+
+            if (ArvoreVazia())
+            {
+                this.Pai = noInserir;
+                return;
+            }
+            Dado dado = noInserir.Dado;
+
+            if (noAtual.Equals(dado))
+            {
+                noAtual.Esquerdo = noInserir;
+                return;
+            }
+
+
+            Lado lado = Operador.LadoEscolher(dado, noAtual);
+            bool inserir = false;
+            if ((noAtual.Esquerdo is null && lado == Lado.Esquerdo) || (noAtual.Direito is null && lado == Lado.Direito))
+            {
+                inserir = true;
+            }
+            switch (lado)
+            {
+                case Lado.Direito:
+                    if (inserir)
+                    {
+                        noAtual.Direito = new No(dado);
+                        break;
+                    }
+                    InserirNo(noInserir, noAtual.Direito);
+                    break;
+                case Lado.Esquerdo:
+                    if (inserir)
+                    {
+                        noAtual.Esquerdo = new No(dado);
+                        break;
+                    }
+                    InserirNo(noInserir, noAtual.Esquerdo);
+                    break;
+                default: break;
+            }
+
+            return;
+        }
+
+
+        public void PercorreArvore(No noAtual)
+        {
+            if (ArvoreVazia())
+            {
+                throw new ArgumentException("Arvore está vazia.");
+            }
+            Console.WriteLine(noAtual);
+
+            if (!(noAtual.Esquerdo is null))
+            {
+                PercorreArvore(noAtual.Esquerdo);
+            }
+            if (!(noAtual.Direito is null))
+            {
+                PercorreArvore(noAtual.Direito);
+            }
+
+        }
+
+        public bool ArvoreVazia()
+        {
+            return this.Pai is null;
+        }
+
+        public override string ToString()
+        {
+            return this.Pai.ToString();
+        }
+
     }
-  
-    public bool ArvoreVazia() 
-    {
-      return this.Pai is null;
-    }
-
-    public override string ToString()
-    {
-      return this.Pai.ToString();
-    }
-    
-  }
 
 }
--- Implementação Árvore Binária/Operations/Operador.cs
+++ Implementação Árvore Binária/Operations/Operador.cs
@@ -2,24 +2,25 @@ using System;
 using Estruturas;
 using Enums;
 
-namespace Operations 
+namespace Operations
 {
-  public static class Operador 
-  {
-
-    public static Lado LadoEscolher (Dado dado, No no) 
+    public static class Operador
     {
 
-      // Caso esquerdo
-      if( no.Dado.Numero > dado.Numero )
-      {
-        return Lado.Esquerdo;
-      } else
-      {
-        return Lado.Direito;
-      }
-      
-    }
-    
-  } // Classe
+        public static Lado LadoEscolher(Dado dado, No no)
+        {
+
+            // Caso esquerdo
+            if (no.Dado.Numero > dado.Numero)
+            {
+                return Lado.Esquerdo;
+            }
+            else
+            {
+                return Lado.Direito;
+            }
+
+        }
+
+    } // Classe
 } // Namespace
